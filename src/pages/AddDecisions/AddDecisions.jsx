cfb7ac4c404be644a903fe063c65b68ed6307748
Chris Ko <ko_ka_chun@hotmail.com>
2022-09-02 19:23:18 +0100

    added a blackCross image and Link back to Home to AddDecision.

--- src/pages/AddDecisions/AddDecisions.jsx
+++ src/pages/AddDecisions/AddDecisions.jsx
@@ -1,9 +1,18 @@
+import blackCross from "../../assets/images/black-cross.png";
 import Form from "../../components/Form/Form";
 import "./AddDecisions.scss";
+import { Link } from "react-router-dom";
 
 const AddDecision = () => {
   return (
     <div className="addDecision">
+      <Link to="/react-decision-making">
+        <img
+          src={blackCross}
+          className="addDecision addDecision__black-cross"
+          alt="black cross"
+        />
+      </Link>
       <h1 className="addDecision addDecision__heading">
         Add decisions you have made
       </h1>
