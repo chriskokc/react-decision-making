e785c71b2fa3ad790828509b342d399721105177
Chris Ko <ko_ka_chun@hotmail.com>
2022-09-02 17:18:43 +0100

    added variables and applied basic styles to text and background.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,15 @@
+import "./Form.scss";
+
 const Form = ({ title, isTitleForm }) => {
   return (
     <form className="form">
-      <p className="form__heading">
+      <p className="form form__heading">
         <label htmlFor={`${title}-form`}>{title}:</label>
       </p>
       <textarea
         name={`${title}-form`}
         className={`form__${title}`}
-        cols="50"
+        cols="40"
         rows={isTitleForm ? "8" : "20"}
       ></textarea>
     </form>
