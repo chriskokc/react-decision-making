de02371b908dd2d647e7ae72cd9cc91828bc0aa1
Chris Ko <ko_ka_chun@hotmail.com>
2022-09-02 17:53:14 +0100

    created route to Journey and added styles to Home.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddDecision from "./pages/AddDecisions/AddDecisions";
 import Home from "./pages/Home/Home";
+import Journey from "./pages/Journey/Journey";
 
 import "./styles/main.scss";
 
@@ -9,6 +10,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/react-decision-making/" element={<Home />} />
+        <Route path="/react-decision-making/journey" element={<Journey />} />
         <Route
           path="/react-decision-making/addDecisions"
           element={<AddDecision />}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,18 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
-      <h1 className="home__title">Welcome to Insights</h1>
-      <h2>Journey</h2>
-      <Link to="/react-decision-making/addDecisions" className="home__link">
-        Add decisions for the day
-      </Link>
-      <h2>For future self</h2>
-      <h2>Sharing with others</h2>
+      <h1 className="home home__title">Welcome to Insights</h1>
+      <div className="home home__main">
+        <Link to="/react-decision-making/journey" className="home home__link">
+          Journey
+        </Link>
+        <Link
+          to="/react-decision-making/addDecisions"
+          className="home home__link"
+        >
+          Add decisions for the day
+        </Link>
+      </div>
     </div>
   );
 };
