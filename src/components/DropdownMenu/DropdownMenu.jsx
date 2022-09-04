4cd100571e80b428f2a0ece124e9d283c92b096c
Chris Ko <ko_ka_chun@hotmail.com>
2022-09-04 14:23:46 +0100

    added styles to DropdownMenu.

--- src/components/DropdownMenu/DropdownMenu.jsx
+++ src/components/DropdownMenu/DropdownMenu.jsx
@@ -3,7 +3,9 @@ import "./DropdownMenu.scss";
 const DropdownMenu = () => {
   return (
     <form className="dropDownMenu">
-      <label htmlFor="type">Choose a type: </label>
+      <label htmlFor="type" className="dropDownMenu__label">
+        Choose a type:{" "}
+      </label>
       <select name="type" id="type" className="dropDownMenu__options">
         <option value="life">Life</option>
         <option value="work">Work</option>
