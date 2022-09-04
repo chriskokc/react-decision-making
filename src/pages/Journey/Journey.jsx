93eb13c691127e5b4d8e330f8c4c32238a8f8b1b
Chris Ko <ko_ka_chun@hotmail.com>
2022-09-04 14:42:55 +0100

    added styles to Journey's heading and black-cross.

--- src/pages/Journey/Journey.jsx
+++ src/pages/Journey/Journey.jsx
@@ -1,17 +1,27 @@
+import "./Journey.scss";
+import blackCross from "../../assets/images/black-cross.png";
+import { Link } from "react-router-dom";
+
 const Journey = () => {
   // GET
-  const getUserData = async () => {
-    const url = "http://localhost:8080/decisions";
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    return data;
-  };
+  // const getUserData = async () => {
+  //   const url = "http://localhost:8080/decisions";
+  //   const response = await fetch(url);
+  //   const data = await response.json();
+  //   console.log(data);
+  //   return data;
+  // };
 
   return (
-    <div>
-      <h1>Journey</h1>
-      <button onClick={() => getUserData()}>Click</button>
+    <div className="journey">
+      <Link to="/react-decision-making">
+        <img
+          src={blackCross}
+          className="journey__black-cross"
+          alt="black cross"
+        />
+      </Link>
+      <h1 className="journey__heading">Journey</h1>
     </div>
   );
 };
