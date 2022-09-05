6d5f48d8cec57592fe4e4ba353603149bd64426c
Chris Ko <ko_ka_chun@hotmail.com>
2022-09-05 19:14:41 +0100

    put in userData to EditDecisions.

--- src/pages/EditDecisions/EditDecisions.jsx
+++ src/pages/EditDecisions/EditDecisions.jsx
@@ -2,11 +2,22 @@ import "./EditDecisions.scss";
 import blackCross from "../../assets/images/black-cross.png";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import DecisionCard from "../../components/DecisionCard/DecisionCard";
 import Button from "../../components/Button/Button";
+import { useContext } from "react";
+import DecisionContext from "../../context/DecisionContext";
 
 const EditDecisions = () => {
-  const parameters = useParams();
-  console.log(parameters);
+  const { decisionId } = useParams();
+  const { userData } = useContext(DecisionContext);
+
+  const foundDecision = () => {
+    const result = userData.filter((decision) => {
+      return decision.id === Number(decisionId);
+    });
+    return result;
+  };
+
   return (
     <div className="editDecision">
       <Link to="/react-decision-making">
@@ -16,8 +27,13 @@ const EditDecisions = () => {
           alt="black cross"
         />
       </Link>
-      <h1 className="editDecision__heading">Decision</h1>
-
+      <h1 className="editDecision__heading">Decisions saved</h1>
+      <DecisionCard
+        type={foundDecision()[0].type}
+        title={foundDecision()[0].title}
+        content={foundDecision()[0].content}
+        date={foundDecision()[0].dateCreated.split("T")[0]}
+      />
       <Button buttonText="edit" />
     </div>
   );
